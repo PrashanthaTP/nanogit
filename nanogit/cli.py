0c7998fdd228b10de92092c179cfa3bac79caa3d
PrashanthaTP <tpprashantha@gmail.com>
2025-06-24 21:53:49 +0530

    Feat: Implement hash-object
    + Related: #5

--- nanogit/cli.py
+++ nanogit/cli.py
@@ -9,6 +9,10 @@ def init(args):
     logger.info("init called")
     core.init()
 
+def hash_object(args):
+    with open(args.file,'rb') as file:
+        print(core.hash_object(file.read()))
+
 def parse_args():
     parser = argparse.ArgumentParser(prog="Nano Git",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -17,6 +21,10 @@ def parse_args():
     init_parser = commands.add_parser("init")
     init_parser.set_defaults(func=init)
 
+    hash_object_parser = commands.add_parser("hash-object")
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument("file")
+    
     return parser.parse_args()
 
 def main():
--- nanogit/core.py
+++ nanogit/core.py
@@ -1,11 +1,20 @@
 import os
+import hashlib
 
 from nanogit.utils import get_logger
 logger = get_logger()
 
 GIT_DIR=".ngit"
-
+OBJECT_DIR=os.path.join(GIT_DIR,"objects")
 #TODO: create git direcotry in specified directory - take it as an argument
 def init():
     os.makedirs(GIT_DIR,exist_ok=True)
+    os.makedirs(OBJECT_DIR,exist_ok=True)
     logger.info("Initialized empty .ngit repository in %s"%(os.getcwd()))
+
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(os.path.join(OBJECT_DIR,oid),'wb') as file:
+        file.write(data)
+    return oid
+    
