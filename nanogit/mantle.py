c6b36053e42954c33e443899afa483edd5f4a758
PrashanthaTP <tpprashantha@gmail.com>
2025-07-01 22:03:19 +0530

    Feat: Update write-tree command
    + Possibility to ignore paths
    + Related : #7

--- nanogit/mantle.py
+++ nanogit/mantle.py
@@ -1,9 +1,16 @@
 import os
 
+def should_ignore_path(filepath):
+    #TODO:; get git directory name from a single location
+    return '.ngit' in filepath.split(os.sep)
+
 def write_tree(directory="."):
     with os.scandir(directory) as it:
         for entry in it:
             full = os.path.join(directory,entry.name)
+            if should_ignore_path(full):
+                print(full)
+                continue
             if entry.is_file(follow_symlinks=False):
                 print(full)#TODO: update object store
             elif entry.is_dir(follow_symlinks=False):
